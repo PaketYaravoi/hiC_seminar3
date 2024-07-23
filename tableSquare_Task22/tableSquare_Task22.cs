baa6f87d476c9821facd4858788421250aac2728
PaketYaravoi <roma.klimov10.94@gmail.com>
2024-07-24 10:14:52 +1100

    Создал задачу: Таблица квадратов от 1 до N(Task22)

---
+++ tableSquare_Task22/tableSquare_Task22.cs
@@ -0,0 +1,49 @@
+﻿//адача 22. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+//от 1 до N.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 0;
+
+        Console.Write("Enter integer N: ");
+
+        while(!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.Write("inpyt error, please restart enter :");
+        }
+        
+        GetTableSquare(number);
+    }
+    
+    static void GetTableSquare(int num)
+    {
+        for (int i = 1; i <= num -1; i++)
+        {
+            Console.Write($"{i * i}, ");
+        }
+        Console.WriteLine($"{num * num}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
