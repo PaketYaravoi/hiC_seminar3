17bf50d6b170b5348aeddcf3b396eca0287ca8c4
PaketYaravoi <roma.klimov10.94@gmail.com>
2024-07-23 18:06:11 +1100

    Создал программу проверки числа на палиндром: Task19

---
+++ PalindromOrNot_task19/PalindromOrNot.cs
@@ -0,0 +1,45 @@
+﻿
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом?
+
+int number = 0;
+Console.WriteLine("enter five-digit:");
+
+while (!int.TryParse(Console.ReadLine(), out number))
+{
+    Console.WriteLine("input error!");
+    Console.Write("Pleas re-enter: ");
+}
+
+
+string resolt = GetRigthString(number);
+GetAnswer(resolt);
+
+
+string GetRigthString(int number)
+{
+    string strFromNum = string.Empty;
+   
+
+    if (number > 99999 || number < 10000)
+    {
+        return null;
+    }
+    else
+    {
+        strFromNum = number.ToString();
+    }
+    return strFromNum;
+
+}
+
+void GetAnswer(string str)
+{
+    if (str[0] == str[4] && str[1] == str[3])
+    {
+        Console.WriteLine("Palindrom!");
+    }
+    else
+    {
+        Console.WriteLine("NOT Palindrom!");
+    }
+}
