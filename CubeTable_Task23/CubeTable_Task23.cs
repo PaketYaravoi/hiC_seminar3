5699c95119ffe9ebad1eeb271123cc27b0d021f6
PaketYaravoi <roma.klimov10.94@gmail.com>
2024-07-25 16:43:32 +1100

    написал последнюю программу домашнего задания, модифицировал задачу проверки на полидром.

---
+++ CubeTable_Task23/CubeTable_Task23.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу которая принимает на вход число (N) и выводит таблицу кубов чисел от 1 до N.
+
+// 3 --> 1, 8, 27;
+// 5 --> 1, 8, 27, 64, 125;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int inputNum = 0;
+
+        Console.Write("Please, enter integer: ");
+        while (Conver.ToInt32(Console.ReadLine(), out inputNum))
+        {
+            Console.WriteLine("Input error!");
+            Console.Write("Please, restart enter integer: ");
+        }
+
+        PrinTableCube(inputNum);
+
+    }
+
+    static void PrintTableCube
+
+
+
+}
+
--- PalindromOrNot_task19/PalindromOrNot.cs
+++ PalindromOrNot_task19/PalindromOrNot.cs
@@ -1,45 +1,126 @@
 ﻿
-// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом?
+using System;
 
-int number = 0;
-Console.WriteLine("enter five-digit:");
-
-while (!int.TryParse(Console.ReadLine(), out number))
+class Program
 {
-    Console.WriteLine("input error!");
-    Console.Write("Pleas re-enter: ");
+    static void Main()
+    {
+        int number = 0;
+        //while (!int.TryParse(Console.ReadLine(), out number))
+        //{
+        //    Console.WriteLine("input error!");
+        //    Console.Write("Pleas re-enter: ");
+        //}
+
+        while(Convert.ToInt32())
+
+        string strFromNum = GetRigthStr(number);
+
+        IsPalindromOrNot(strFromNum);
+    }
+
+    static string GetRigthStr(int number)
+    {
+        if (number < 0)
+        {
+            return null;
+        }
+        else
+        {
+            return number.ToString();
+        }
+             
+    }
+
+    static void IsPalindromOrNot(string str)
+    {
+        int length = str.Length;
+        bool isPolindrom = true;
+    
+        if (str == null)
+        {
+            Console.WriteLine("-1");
+        }
+        else
+        { 
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (str[i] != str[length - 1 - i])
+                { 
+                    isPolindrom = false;
+                    break;
+                }
+            }
+
+            if (isPolindrom)
+            {
+                Console.WriteLine("Palindrom!");
+            }
+            else
+            {
+                Console.WriteLine("NOT palindrom!");
+            }
+        }
+    }
 }
 
 
-string resolt = GetRigthString(number);
-GetAnswer(resolt);
 
 
-string GetRigthString(int number)
-{
-    string strFromNum = string.Empty;
-   
 
-    if (number > 99999 || number < 10000)
-    {
-        return null;
-    }
-    else
-    {
-        strFromNum = number.ToString();
-    }
-    return strFromNum;
+// Напишите программу, которая принимает на вход (пятизначное( я усложнил задчу)) любое число и проверяет, является ли оно палиндромом?
 
-}
+//int number = 0;
+//Console.WriteLine("enter five-digit:");
 
-void GetAnswer(string str)
-{
-    if (str[0] == str[4] && str[1] == str[3])
-    {
-        Console.WriteLine("Palindrom!");
-    }
-    else
-    {
-        Console.WriteLine("NOT Palindrom!");
-    }
-}
+//while (!int.TryParse(Console.ReadLine(), out number))
+//{
+//    Console.WriteLine("input error!");
+//    Console.Write("Pleas re-enter: ");
+//}
+
+
+//string resolt = GetRigthString(number);
+
+//IsPalindromOrNot(resolt);
+
+
+//string GetRigthString(int number)
+//{
+//    if (number < 0)
+//    {
+//        return null; // отрицательные числа не являются полиндромом
+//    }
+//    else return number.ToString();
+//}
+
+//void IsPalindromOrNot(string str)
+//{
+//    if (str == null)
+//    {
+//        Console.WriteLine("Error: The number is not a polindrom!");
+//        return;
+//    }
+
+//    int length = str.Length;
+//    bool isPolindrom = true;
+
+//    for (int i = 0; i < length / 2; i++)
+//    {
+//        if (str[i] != str[length - i - 1])
+//        {
+//            isPolindrom = false; break;
+//        }
+        
+//    }
+//    if (isPolindrom)
+//    {
+//        Console.WriteLine("Palindrom!");
+//    }
+//    else
+//    {
+//        Console.WriteLine("NOT Palindrom!");
+//    }
+
+//}
