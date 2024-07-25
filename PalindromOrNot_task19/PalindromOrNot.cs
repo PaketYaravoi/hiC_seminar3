307d68849dd76ed2240a94928051c2716af620d5
PaketYaravoi <roma.klimov10.94@gmail.com>
2024-07-25 17:23:55 +1100

    push in remote

--- PalindromOrNot_task19/PalindromOrNot.cs
+++ PalindromOrNot_task19/PalindromOrNot.cs
@@ -6,13 +6,12 @@ class Program
     static void Main()
     {
         int number = 0;
-        //while (!int.TryParse(Console.ReadLine(), out number))
-        //{
-        //    Console.WriteLine("input error!");
-        //    Console.Write("Pleas re-enter: ");
-        //}
+        while (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("input error!");
+            Console.Write("Pleas re-enter: ");
 
-        while(Convert.ToInt32())
+        }
 
         string strFromNum = GetRigthStr(number);
 
@@ -68,59 +67,3 @@ class Program
 
 
 
-
-// Напишите программу, которая принимает на вход (пятизначное( я усложнил задчу)) любое число и проверяет, является ли оно палиндромом?
-
-//int number = 0;
-//Console.WriteLine("enter five-digit:");
-
-//while (!int.TryParse(Console.ReadLine(), out number))
-//{
-//    Console.WriteLine("input error!");
-//    Console.Write("Pleas re-enter: ");
-//}
-
-
-//string resolt = GetRigthString(number);
-
-//IsPalindromOrNot(resolt);
-
-
-//string GetRigthString(int number)
-//{
-//    if (number < 0)
-//    {
-//        return null; // отрицательные числа не являются полиндромом
-//    }
-//    else return number.ToString();
-//}
-
-//void IsPalindromOrNot(string str)
-//{
-//    if (str == null)
-//    {
-//        Console.WriteLine("Error: The number is not a polindrom!");
-//        return;
-//    }
-
-//    int length = str.Length;
-//    bool isPolindrom = true;
-
-//    for (int i = 0; i < length / 2; i++)
-//    {
-//        if (str[i] != str[length - i - 1])
-//        {
-//            isPolindrom = false; break;
-//        }
-        
-//    }
-//    if (isPolindrom)
-//    {
-//        Console.WriteLine("Palindrom!");
-//    }
-//    else
-//    {
-//        Console.WriteLine("NOT Palindrom!");
-//    }
-
-//}
