b188dba22600dc9e26235867e78e98d56ed5a276
PaketYaravoi <roma.klimov10.94@gmail.com>
2024-07-26 17:03:03 +1100

    weekend

--- StrInArr_Task3/StrInArr_Task3.cs
+++ StrInArr_Task3/StrInArr_Task3.cs
@@ -2,17 +2,34 @@
 
 int[] inputNew = input.Split().Select(int.Parse).ToArray(); //можно передать как массив строки введеный по умолчанию(фиксированная строка)
 
-int[] resolt  = Console.ReadLine()!.Split(", ").Select(int.Parse).ToArray();     // так и массив строк введеный вручную (ReadLine())
+int[] resolt = Console.ReadLine()!.Split("; ").Select(int.Parse).ToArray();     // так и массив строк введеный вручную (ReadLine())
 
-                                                    // по умолчаюнию Сплит увидит массив в строке или при вводе нужно указать параметр разделителя
-                                                    // если ошибится при вводе , он просто склеит их , так как не увидит разделителных знаков
-                                                    //                    Parse - им из массива строк в  цело-ый  массив данных (.ToArray)
+                                                        // по умолчаюнию Сплит увидит массив в строке или при вводе нужно указать параметр разделителя
+                                                        // если ошибится при вводе , он просто склеит их , так как не увидит разделителных знаков
+                                                        //                    Parse - им из массива строк в  цело-ый  массив данных (.ToArray)
+Console.WriteLine();
+
+foreach (int i in inputNew)               // можно вложить resolt можно вложить inputNew, только не строку input) так как это строка а не мосивы данных как в случае с inputNew
+{
+    Console.Write(i + "; ");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+for (int i = 0; i < inputNew.Length; i++)
+{
+    Console.Write(inputNew[i] + "; ");
+}
+
+
 Console.WriteLine();
 
 foreach (int i in resolt)               // можно вложить resolt можно вложить inputNew, только не строку input) так как это строка а не мосивы данных как в случае с inputNew
 {
     Console.Write(i + "; ");
 }
+
 Console.WriteLine();
 Console.WriteLine();
 
@@ -20,3 +37,4 @@ for (int i = 0; i < resolt.Length; i++)
 {
     Console.Write(resolt[i] + "; ");
 }
+
